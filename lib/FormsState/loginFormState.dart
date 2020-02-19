d9b2d5cd7e353b13b27fffae35e3499fdd195bad
Vaibhav Sharma <vaibhav.sharma@cgt.co.in>
2020-02-19 21:05:30 +0530

    created and validated the login, registration form

--- lib/FormsState/loginFormState.dart
+++ lib/FormsState/loginFormState.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
-
-// FormField
-import 'package:passwordfield/passwordfield.dart';
+import 'package:flutter/widgets.dart';
 
 // Form
 import 'package:flutter_training_app/Forms/loginForm.dart';
@@ -10,30 +8,48 @@ import 'package:flutter_training_app/Forms/loginForm.dart';
 // Validators
 import 'package:flutter_training_app/validators/textFieldValidators.dart';
 
-
-
 // Create a corresponding State class.
 // This class holds data related to the form.
 class LoginFormState extends State<LoginForm> {
   // Create a global key that uniquely identifies the Form widget
   // and allows validation of the form.
 
+  @override
+  void initState(){
+    super.initState();
+    print("inside initState");
+  }
+
   // Note: This is a GlobalKey<FormState>, not a GlobalKey<LoginFormState>.
   final _formKey = GlobalKey<FormState>();
+  FocusNode txtEmail = new FocusNode();
+  FocusNode txtPassword = new FocusNode();
 
   TextFieldValidators txtFieldValidators = new TextFieldValidators();
 
   // Summary: this function is called when we click on login button.
   void submitForm(){
+
     if (_formKey.currentState.validate()) {
       // If the form is valid, display a Snackbar.
-      Scaffold.of(context)
-          .showSnackBar(SnackBar(content: Text('Processing Data')));
+      Scaffold.of(context).showSnackBar(SnackBar(content: Text('Processing Data')));
     }
+  }
+
+  void validateOnFieldSubmitted(String value, String field) {
+
+    var validate = this.txtFieldValidators.validateFieldValue(value, field);
+//    if(validate == null){
+//      _formKey.currentState.validate();
+      FocusScope.of(context).requestFocus(txtPassword);
+//    }else{
+//      FocusScope.of(context).requestFocus(txtEmail);
+//    }
   }
 
   @override
   Widget build(BuildContext context) {
+    print("inside build");
     // Build a Form widget using the _formKey created above.
     return Form(
       key: _formKey,
@@ -47,20 +63,24 @@ class LoginFormState extends State<LoginForm> {
                         decoration: const InputDecoration(
                             labelText: 'Enter Email'
                         ),
+                        focusNode: txtEmail,
+                        keyboardType: TextInputType.emailAddress,
+                        onFieldSubmitted:(value) => this.validateOnFieldSubmitted(value, 'email'),
                         validator: (value) => this.txtFieldValidators.validateFieldValue(value, 'email')
                       ),
                       TextFormField(
                         decoration: const InputDecoration(
                             labelText: 'Enter Password'
                         ),
+                        focusNode: txtPassword,
                         obscureText: true,
                         validator: (value) => this.txtFieldValidators.validateFieldValue(value, 'password')
                       ),
                       Container(
                         margin: EdgeInsets.only(top: 20),
                         child: ButtonTheme(
-                          minWidth: 120,
-                          height:30,
+                          minWidth: 300,
+                          height:50,
                           child: FlatButton(
                             color: Colors.pink[500],
                             textColor: Colors.white,
@@ -80,8 +100,8 @@ class LoginFormState extends State<LoginForm> {
                       Container(
                         margin: EdgeInsets.only(top: 20),
                         child: ButtonTheme(
-                          minWidth: 120,
-                          height: 30,
+                          minWidth: 300,
+                          height: 50,
                           child: FlatButton(
                               color: Colors.pink[500],
                               textColor: Colors.white,
@@ -90,6 +110,7 @@ class LoginFormState extends State<LoginForm> {
                               padding: EdgeInsets.all(8.0),
                               splashColor: Colors.blueAccent,
                               onPressed: () {
+                                _formKey.currentState.reset();
                                 Navigator.pushNamed(context, '/registration');
                               },
                               child: Text(
--- lib/FormsState/registrationFormState.dart
+++ lib/FormsState/registrationFormState.dart
@@ -1,4 +1,9 @@
+//import 'dart:html';
+import 'dart:io';
 import 'package:flutter/material.dart';
+
+// Dependency Elements
+import 'package:image_picker/image_picker.dart';
 
 // Form
 import 'package:flutter_training_app/Forms/registrationForm.dart';
@@ -8,96 +13,135 @@ import 'package:flutter_training_app/validators/textFieldValidators.dart';
 
 class RegistrationFormState extends State<RegistrationForm>{
 
-  final _formKey = GlobalKey<FormState>();
-  TextFieldValidators txtFieldValidators =new TextFieldValidators();
+    File _pickedImage;
+    final _formKey = GlobalKey<FormState>();
+    TextFieldValidators txtFieldValidators =new TextFieldValidators();
 
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Form(
-        key: _formKey,
-        child: Center(
-            child:Container(
-                margin: EdgeInsets.all(20.0),
-                child: Column(
-                    // crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
+    void submitForm(){
 
-                        TextFormField(
-                            decoration: const InputDecoration(
-                                labelText: 'Enter Your Name'
-                            ),
-                            validator: (value) => this.txtFieldValidators.validateFieldValue(value, 'name')
-                        ),
-                        TextFormField(
-                            decoration: const InputDecoration(
-                                labelText: 'Enter Your Username'
-                            ),
-                            validator: (value) => this.txtFieldValidators.validateFieldValue(value, 'username')
-                        ),
-                        TextFormField(
-                            decoration: const InputDecoration(
-                                labelText: 'Enter Email'
-                            ),
-                            validator: (value) => this.txtFieldValidators.validateFieldValue(value, 'email')
-                        ),
-                        TextFormField(
-                            decoration: const InputDecoration(
-                                labelText: 'Enter Password'
-                            ),
-                            obscureText: true,
-                            validator: (value) => this.txtFieldValidators.validateFieldValue(value, 'password')
-                        ),
-                        Container(
-                            margin: EdgeInsets.only(top: 20),
-                            child: ButtonTheme(
-                                minWidth: 120,
-                                height:30,
-                                child: FlatButton(
-                                    color: Colors.pink[500],
-                                    textColor: Colors.white,
-                                    disabledColor: Colors.grey,
-                                    disabledTextColor: Colors.black,
-                                    padding: EdgeInsets.all(8.0),
-                                    splashColor: Colors.blueAccent,
+        if(_pickedImage == null) {
+            this._pickImage();
+        }
+        if(_formKey.currentState.validate()) {
+            Scaffold.of(context).showSnackBar(SnackBar(content: Text('Processing data')));
+        }
+    }
 
-                                    onPressed: (){
-
-                                    },
-//                                    => this.submitForm(),
-                                    child: Text(
-                                        "Login",
-                                        style: TextStyle(fontSize: 20.0),
-                                    ),
-                                ),
-                            ),
+    void _pickImage() async {
+        final imageSource = await showDialog<ImageSource>(
+            context: context,
+            builder: (context) =>
+                AlertDialog(
+                    title: Text("Select the image source"),
+                    actions: <Widget>[
+                        MaterialButton(
+                            child: Text("Camera"),
+                            onPressed: () => Navigator.pop(context, ImageSource.camera),
                         ),
-                        Container(
-                            margin: EdgeInsets.only(top: 20),
-                            child: ButtonTheme(
-                                minWidth: 120,
-                                height: 30,
-                                child: FlatButton(
-                                    color: Colors.pink[500],
-                                    textColor: Colors.white,
-                                    disabledColor: Colors.grey,
-                                    disabledTextColor: Colors.black,
-                                    padding: EdgeInsets.all(8.0),
-                                    splashColor: Colors.blueAccent,
-                                    onPressed: () {
-                                        Navigator.pushNamed(context, '/registration');
-                                    },
-                                    child: Text(
-                                        'Registration',
-                                        style: TextStyle(fontSize: 20.0),
-                                    )
-                                ),
-                            ),
+                        MaterialButton(
+                            child: Text("Gallery"),
+                            onPressed: () => Navigator.pop(context, ImageSource.gallery),
                         )
                     ],
-                ),
-            )
-        ),
+                )
+        );
+
+        if(imageSource != null) {
+            final file = await ImagePicker.pickImage(source: imageSource);
+            if(file != null) {
+                setState(() => _pickedImage = file as File);
+            }
+        }
+    }
+
+    @override
+    Widget build(BuildContext context) {
+    // TODO: implement build
+    return Form(
+    key: _formKey,
+    child: Center(
+        child:Container(
+            margin: EdgeInsets.all(20.0),
+            child: Column(
+                // crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+
+                    Center(
+                        child: Column(
+                            children: <Widget>[
+                                _pickedImage == null ? Text("No image selected"): Image(height: 100, width: 100, image: FileImage(_pickedImage)),
+                                Container(
+                                    margin: EdgeInsets.only(top: 20),
+                                    child: ButtonTheme(
+                                        minWidth: 120,
+                                        height: 30,
+                                        child: FlatButton(
+                                            color: Colors.pink[500],
+                                            textColor: Colors.white,
+                                            disabledColor: Colors.grey,
+                                            disabledTextColor: Colors.black,
+                                            padding: EdgeInsets.all(8.0),
+                                            splashColor: Colors.blueAccent,
+                                            onPressed:() => this._pickImage(),
+                                            child: Text(
+                                                'Pick Image',
+                                                style: TextStyle(fontSize: 20.0),
+                                            )
+                                        ),
+                                    ),
+                                )
+                            ],
+                        )
+                    ),
+                    TextFormField(
+                        decoration: const InputDecoration(
+                            labelText: 'Enter Your Name'
+                        ),
+                        validator: (value) => this.txtFieldValidators.validateFieldValue(value, 'name')
+                    ),
+                    TextFormField(
+                        decoration: const InputDecoration(
+                            labelText: 'Enter Your Username'
+                        ),
+                        validator: (value) => this.txtFieldValidators.validateFieldValue(value, 'username')
+                    ),
+                    TextFormField(
+                        decoration: const InputDecoration(
+                            labelText: 'Enter Email'
+                        ),
+                        validator: (value) => this.txtFieldValidators.validateFieldValue(value, 'email')
+                    ),
+                    TextFormField(
+                        decoration: const InputDecoration(
+                            labelText: 'Enter Password'
+                        ),
+                        obscureText: true,
+                        validator: (value) => this.txtFieldValidators.validateFieldValue(value, 'password')
+                    ),
+                    Container(
+                        margin: EdgeInsets.only(top: 20),
+                        child: ButtonTheme(
+                            minWidth: 300,
+                            height:30,
+                            child: FlatButton(
+                                color: Colors.pink[500],
+                                textColor: Colors.white,
+                                disabledColor: Colors.grey,
+                                disabledTextColor: Colors.black,
+                                padding: EdgeInsets.all(8.0),
+                                splashColor: Colors.blueAccent,
+                                onPressed: ()=> this.submitForm(),
+                                child: Text(
+                                    "Register",
+                                    style: TextStyle(fontSize: 20.0),
+                                ),
+                            ),
+                        ),
+                    ),
+                ],
+            ),
+        )
+    ),
 
     );
   }
